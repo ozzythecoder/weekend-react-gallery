8369094801d58f4d411de8f13f691e1100d64cae
August McAllister <august.chm@gmail.com>
2022-11-24 12:02:42 -0600

    moved gallery placeholder to GalleryList component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from '../Header/Header';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
     return (
       <div className="App">
         <Header />
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <GalleryList />
+
       </div>
     );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,9 @@
+export default function GalleryList() {
+
+  return (
+    <div>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg"/>
+    </div>
+  )
+}
