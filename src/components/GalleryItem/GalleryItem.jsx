73074e61e230a2b1b6be3c8f41f5dbe55395a70c
August McAllister <august.chm@gmail.com>
2022-11-24 13:36:11 -0600

    implemented blur on mousehover

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,10 +2,16 @@ import './GalleryItem.css'
 
 export default function GalleryItem({img}) {
 
+  const toggleDescription = () => {
+    console.log('in toggleDesc');
+  }
+
   return (
     <div>
       <div className="gallery-item">
-        <img src={img} />
+        <img src={img} 
+          onClick={toggleDescription}
+        />
       </div>
     </div>
   )
