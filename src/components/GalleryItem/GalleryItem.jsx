895a73f8383422f01e7ed6eaf995fb5a2b7138dd
August McAllister <august.chm@gmail.com>
2022-11-24 13:55:44 -0600

    added mouseover and mouseout to image divs

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import './GalleryItem.css'
 
 export default function GalleryItem({img}) {
+
+  const [ isHover, setIsHover ] = useState(false)
+      
+  const handleMouseOver = () => {
+    setIsHover(true)
+  }
+
+  const handleMouseOut = () => {
+    setIsHover(false)
+  }
 
   const toggleDescription = () => {
     console.log('in toggleDesc');
@@ -9,9 +20,13 @@ export default function GalleryItem({img}) {
   return (
     <div>
       <div className="gallery-item">
-        <img src={img} 
-          onClick={toggleDescription}
-        />
+        <div className="img-holder">
+          <img src={img} 
+            onClick={toggleDescription}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            />
+        </div>
       </div>
     </div>
   )
