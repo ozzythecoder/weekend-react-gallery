4cb4959eefb3efc42c774b9b53f4f2ccf83d949a
August McAllister <august.chm@gmail.com>
2022-11-24 13:29:18 -0600

    basic CSS alignment for working product

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -7,6 +7,10 @@ export default function GalleryList() {
     <div>
       <div className="gallery-list">
         <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
         <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
       </div>
     </div>
