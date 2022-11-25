1d150aa9ebd3a1316317b35a200037008a999160
August McAllister <august.chm@gmail.com>
2022-11-25 11:41:26 -0600

    set up get request and gallery item loop in galleryList

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,7 +1,38 @@
+import Axios from "axios"
+import { useEffect, useState } from "react"
+
 import GalleryItem from "../GalleryItem/GalleryItem"
 import './GalleryList.css'
 
 export default function GalleryList() {
+
+  useEffect(() => {
+    getList();
+  }, [])
+
+  const [ content, setContent ] = useState([]);
+
+  const getList = () => {
+    Axios.get('/gallery')
+      .then(res => {
+        console.log('got gallery items');
+        setContent(res.data);
+      })
+      .catch(err => {
+        console.log('error getting gallery items');
+      })
+  }
+
+  const galleryItems = content.map(item => {
+      return (
+        <GalleryItem
+          className="flex-child"
+          key={item.id}
+          img={item.path}
+          description={item.description}
+          likes={item.likes} />
+      )
+    })
 
   return (
     <div>
