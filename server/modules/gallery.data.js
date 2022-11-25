db4b9d5d80c063750d60c8ffd86834a4b8409edd
August McAllister <august.chm@gmail.com>
2022-11-25 11:29:25 -0600

    Updated gallery.data module to include new pictures, descriptions and like counts

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/bar_small.jpg', description: 'A warmly lit bar, with many shelves of fine spirits.', likes: 3},
+    { id: 2, path: 'images/beach_small.jpg', description: 'A beautiful beach, a few hours before sunset.', likes: 10},
+    { id: 3, path: 'images/mountain-goats_small.jpg', description: 'Three mountain goats at the summit of a large mountain.', likes: 14},
+    { id: 4, path: 'images/parthenon_small.jpg', description: 'The Greek Parthenon, against a backdrop of a bright blue sky.', likes: 9},
+    { id: 5, path: 'images/soccer_small.jpg', description: 'A soccer game from the sidelines – the offense is attempting a goal.', likes: 2},
+    { id: 6, path: 'images/winter-city_small.jpg', description: 'A dense, yet peaceful city block, after a heavy snowfall.', likes: 8}
 ];
 
 module.exports = galleryItems;
