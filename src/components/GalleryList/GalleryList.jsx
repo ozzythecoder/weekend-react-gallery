76ad14e2da015f486e158d0beb3f7e86f6e03724
August McAllister <august.chm@gmail.com>
2022-11-24 23:01:17 -0600

    Minor styling adjustments before bed

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -7,11 +7,11 @@ export default function GalleryList() {
     <div>
       <div className="gallery-list">
         <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
-        <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
-        <GalleryItem className="flex-child" img="./images/bar_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/beach_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/parthenon_small.jpg" />
         <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
-        <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
-        <GalleryItem className="flex-child" img="./images/winter-city_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/soccer_small.jpg" />
+        <GalleryItem className="flex-child" img="./images/mountain-goats_small.jpg" />
       </div>
     </div>
   )
